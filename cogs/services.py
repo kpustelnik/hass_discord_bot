c1af6301d3a6d7ac5f9e99deb18ec5b3b65648f5
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2025-07-28 22:15:53 +0200

    services WIP

--- cogs/services.py
+++ cogs/services.py
@@ -1,33 +1,51 @@
-from typing import Optional, Literal
-
+from typing import Optional, Literal, List
 import discord
 from discord.ext import commands
 from discord import app_commands
+import yaml
+import json
+import inspect
 
 from bot import HASSDiscordBot
 from autocompletes import Autocompletes
 from functools import partial
+from enums.emojis import Emoji
+from models.ServiceModel import DomainModel, ServiceModel
 
 # TODO: Parse object with yaml, and then json
 
 class Services(commands.Cog):
   def __init__(self, bot: HASSDiscordBot) -> None:
     self.bot = bot
-  
-    ha_domains = self.bot.homeassistant_client.cache_custom_get_domains()
-    for domain in ha_domains.values():
-      group = app_commands.Group(
-        name=domain.domain_id,
-        description="Some description",
-        guild_ids=[self.bot.discord_main_guild_id] if self.bot.discord_main_guild_id is not None else None
-      )
 
-      for service in domain.services.values():
-        self.create_service_command(group, domain, service)
-      self.bot.tree.add_command(group)
+    try:
+      ha_domains: List[DomainModel] = self.bot.homeassistant_client.cache_custom_get_domains()
+      for domain in ha_domains:
+        group = app_commands.Group(
+          name=domain.domain,
+          description=f"{domain.domain} services (actions)",
+          guild_ids=[self.bot.discord_main_guild_id] if self.bot.discord_main_guild_id is not None else None
+        )
+
+        for service_id, service in domain.services.items():
+          self.create_service_command(group, domain, service_id, service)
+
+        self.bot.tree.add_command(group)
+    except Exception as e:
+      self.bot.logger.error("Failed to fetch domains and create service action commands")
   
-  def create_service_command(self, group, domain, service):
-    pass
+  def create_service_command(self, group, domain: DomainModel, service_id: str, service: ServiceModel):
+    async def handler(interaction: discord.Interaction, entity_id: str, **kwargs):
+    handler.__name__ = f"{service_id}"  # required to avoid duplicate names
+    handler.__qualname__ = handler.__name__
+    
+    params = [
+        inspect.Parameter(
+            name="interaction",
+            kind=inspect.Parameter.POSITIONAL_OR_KEYWORD,
+            annotation=discord.Interaction
+        ),
+    ]
 
 async def setup(bot: HASSDiscordBot) -> None:
   await bot.add_cog(Services(bot))
